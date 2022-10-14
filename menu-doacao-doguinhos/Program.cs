4187e38a9424942897b337794f253bbb6b8f1d5e
leticia.garz <leticia.garz@BUE0102D13>
2022-10-13 21:44:10 -0300

    Adicionar arquivos de projeto.

---
+++ menu-doacao-doguinhos/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("(1) Usuário (2) Admnistrador");
+string opcaoMenu = Console.ReadLine();
+if (opcaoMenu == "1")
+{
+    // metodo le
+}
+else if (opcaoMenu == "2")
+{
+    // metodo cecilia
+}
+else
+{
+    Console.WriteLine("Opção não autorizada");
+}

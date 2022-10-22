3890a6c9086496d1d8f3749424fff55f51b26911
CECILIA ALICE DOS SANTOS SILVA <cecilia.silva2@alunos.sc.senac.br>
2022-10-21 21:13:18 -0300

    cecilias 2nd commit

--- menu-doacao-doguinhos/MenuAdmin.cs
+++ menu-doacao-doguinhos/MenuAdmin.cs
@@ -3,57 +3,103 @@ namespace menu_doacao_doguinhos
 {
     public class MenuAdmin
     {
-        public int Codigo { get; set; }
-        public int Senha { get; set; }
+        
+
+        public int Admins { get; set; }
         public string Nome { get; set; }
         public int Idade { get; set; }
         public bool Castracao { get; set; }
         public char Sexo { get; set; }
         public char Porte { get; set; }
 
+
         public void OpcaoMenuAdmin()
         {
-            Console.WriteLine("Selecione umas das opções do menu:");
-            Console.WriteLine("1 - Login\n 2 - Cadastrar como administador\n 3 - Voltar ao menu principal\n");
-            string opcaoMenuAdmin = Console.ReadLine();
 
-            if (opcaoMenuAdmin == "1")
+            bool loop = true;
+
+            while (loop)
             {
-                Logar();
-            }
-            if (opcaoMenuAdmin == "2")
-            {
-                CadastrarAdmin();
+                Console.WriteLine("Selecione umas das opções do menu:");
+                Console.WriteLine("1 - Login\n2 - Voltar ao menu principal\n");
+                string opcaoMenuAdmin = Console.ReadLine();
+
+                if (opcaoMenuAdmin == "1")
+                {
+                    Logar();
+                }
+                else if(opcaoMenuAdmin == "2")
+                {
+                    loop = false;
+                }
+                else
+                {
+                    Console.WriteLine("Selecione uma opção válida");
+                }
             }
         }
         public void Logar()
         {
-            if (Codigo == null)
-            {
-                Console.WriteLine("Se cadastre para realizar um login");
-            }
-            else
-            {
                 Console.WriteLine("Digite seu número de user");
                 int UserDigitado = Convert.ToInt32(Console.ReadLine());
                 Console.WriteLine("Digite sua senha");
                 int senhaDigitada = Convert.ToInt32(Console.ReadLine());
 
-                if (UserDigitado != Codigo || senhaDigitada != Senha)
+                if (UserDigitado != 1021 || senhaDigitada != 1312)
                 {
+                    Console.Clear();
                     Console.WriteLine("Acesso não autorizado");
                 }
                 else
                 {
                     Console.WriteLine("Bem-vindo ao sistema");
+                    OpcaoLogin();
+
+                }
+            
+            
+        }
+        public void OpcaoLogin()
+        {
+            bool loop = true;
+
+            while (loop)
+            {
+                Console.WriteLine("Escolha uma ação para continuar:\n 1 - Ver Listagem \n 2 - Cadastrar cães\n 3 - Alterar Status\n 4 - Voltar para login ");
+                string opcaoMenuLogin = Console.ReadLine();
+
+                if(opcaoMenuLogin == "1")
+                {
+                    Listagem();
+                }
+                else if (opcaoMenuLogin == "2")
+                {
+                    CadastrarCaes();
+                }
+                else if (opcaoMenuLogin == "3")
+                {
+                    AlterarStatus();
+                }
+                else if(opcaoMenuLogin == "4")
+                {
+                    loop = false;
+                }
+                else
+                {
+                    Console.WriteLine("Selecione uma opção válida");
                 }
             }
         }
-        public void CadastrarAdmin()
+        public void Listagem()
         {
 
         }
         public void CadastrarCaes()
+        {
+
+
+        }
+        public void AlterarStatus()
         {
 
         }

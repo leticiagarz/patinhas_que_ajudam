e7e222811c5e49a208829692c8a292c0374bffbf
leticia.garz <garzleticia@gmail.com>
2022-10-21 21:33:22 -0300

    6th commit

--- menu-doacao-doguinhos/MenuUsuario.cs
+++ menu-doacao-doguinhos/MenuUsuario.cs
@@ -5,27 +5,12 @@
 		public int Id { get; set; }
 		public string Nome { get; set; }
 		public int Idade { get; set; }
-		public char Porte { get; set; }
-		public char Sexo { get; set; }
+		public string Porte { get; set; }
+		public string Sexo { get; set; }
 		public string Castrado { get; set; }
 		//ver isso
 		public DateTime Entrega { get; set; }
 		public string Status { get; set; }
-		public void CadastrarUsuario()
-		{ 
-			Console.WriteLine("Nome:");
-			Nome = Console.ReadLine();
-			Console.WriteLine("Idade:");
-			Idade = int.Parse(Console.ReadLine());
-			Console.WriteLine("Porte:");
-			Porte = Convert.ToChar(Console.ReadLine());
-			Console.WriteLine("Sexo:");
-			Sexo = Convert.ToChar(Console.ReadLine());
-			Console.WriteLine("Castrado:");
-			Castrado = Console.ReadLine();
-			Console.WriteLine("Data e hora de entrega:");
-			Entrega = Convert.ToDateTime(Console.ReadLine());
-		}
 		public void OpcaoMenuUsuario()
 		{
 			bool l2 = true;
@@ -54,31 +39,107 @@
 				}
 			}
 		}
+		public void CadastrarUsuario()
+		{
+			Console.WriteLine("Nome: ");
+			Nome = Console.ReadLine();
+			Console.WriteLine("Idade: ");
+			Idade = int.Parse(Console.ReadLine());
+			Console.WriteLine("Porte: (P) Pequeno (M) Médio (G) Grande");
+			Porte = Console.ReadLine();
+			Porte = Porte[0].ToString().ToUpper();
+			Console.WriteLine("Sexo: (F) Fêmea (M) Macho");
+			Sexo = Console.ReadLine();
+			Sexo = Sexo.Substring(0, 1);
+			Sexo.ToUpper();
+			Console.WriteLine("Castrado: (S) Sim (N) Não");
+			Castrado = Console.ReadLine();
+			Castrado = Castrado.Substring(0, 1);
+			Castrado.ToUpper();
+			Console.WriteLine("Data e hora de entrega: ");
+			Entrega = Convert.ToDateTime(Console.ReadLine());
+			Status = "À caminho";
+			Id++;
+			Console.Clear();
+			Console.WriteLine("Doguinho cadastrado");
+			
+        }
 		public void VerListagem()
         {
             //for (int i = 0; i < Id; i++)
             //{
             //    Console.WriteLine(arrayUsuario[i]);
             //}
+            Console.WriteLine($"Id: {Id} - Nome: {Nome} - Idade: {Idade} - Porte: {Porte} - Sexo: {Sexo} - Castração: {Castrado} - Entrega: {Entrega} - Status: {Status}");
         }
 		public void Adotar()
         {
-			Console.WriteLine("Digite o código do cão que você deseja adotar ou (A) Voltar ");
-            string opcaoMenuUsuario = Console.ReadLine();
-            if (opcaoMenuUsuario == "A")
-            {
+			//Console.WriteLine("Digite o código do cão que você deseja adotar ou (A) Voltar ");
+   //         string opcaoMenuUsuario = Console.ReadLine();
+   //         if (opcaoMenuUsuario == "A")
+   //         {
 				
-            }
-            else
-			{
+   //         }
+   //         else
+			//{
 				
-            }
+   //         }
         }
         public void Pesquisar()
         {
                 //		Console.WriteLine("Digite o que você deseja pesquisar:");
                 //		string opcaoPesquisar = Console.ReadLine();
                 //for?
+        }
+		public void ValidarSexo()
+        {
+			if (Sexo == "M")
+			{
+				Sexo = "Macho";
+			}
+			else if (Sexo == "F")
+			{
+				Sexo = "Fêmea";
+			}
+			else
+			{
+				Console.WriteLine("Opção inválida");
+			}
+		}
+		public void ValidarPorte()
+        {
+			if (Porte == "P")
+			{
+				Porte = "Pequeno";
+			}
+			else if (Porte == "M")
+			{
+				Porte = "Médio";
+			}
+			else if (Porte == "G")
+			{
+				Porte = "Grande";
+			}
+			else
+			{
+				Console.WriteLine("Opção inválida");
+			}
+		}
+		public void ValidarCastracao()
+        {
+			if (Castrado == "S")
+            {
+				Castrado = "Sim";
+            }
+			else if (Castrado == "N")
+            {
+				Castrado = "Não";
+            }
+			else
+            {
+				Console.WriteLine("Opção inválida");
+			}
+			
         }
 	}
 }
